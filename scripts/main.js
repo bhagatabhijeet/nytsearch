49a19bad169ac5f4ecc4aae359f9b6376c47ab84
Abhijeet Bhagat <bhagat.abhijeet@gmail.com>
2020-08-15 12:11:12 -0700

    add buildURL function

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,20 @@
 let apikey = '3iPm006gtnDlfYwKDbCjLEXo8oabmmeT'
-let queryURLBase ="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=3iPm006gtnDlfYwKDbCjLEXo8oabmmeT";
+let queryURLBase ="https://api.nytimes.com/svc/search/v2/articlesearch.json?q=";//election&api-key=3iPm006gtnDlfYwKDbCjLEXo8oabmmeT";
+//
 let searchTerm="";
-let filters="";
+let searchCount="";
+let startYear="";
+let endYear="";
+
 
 $(document).ready(function(){
     // alert("i am alive");
+    
+    //add event handler to runSearch button
+    $("#runSearch").on("click",searchArticles);
+
+
+
     $.ajax({
         url:queryURLBase,
         method:'GET'
@@ -12,3 +22,50 @@ $(document).ready(function(){
         console.log(data);
     });
 });
+
+
+// this is where the search will happen
+function searchArticles(event){
+    buildURL();
+    event.preventDefault();
+}
+
+function buildURL(){
+    searchTerm=$("#searchTerm").val().trim();
+    searchCount = $("#numRecordsSelect").val();
+    startYear=$("#startYear").val().trim();
+    endYear=$("#endYear").val().trim();
+
+    let returnUrl=`${queryURLBase}${searchTerm}`;
+
+    //user provides no values
+    if(startYear === "" &&  endYear === ""){
+        returnUrl=`${queryURLBase}${searchTerm}&api-key=${apikey}`;
+        return returnUrl;
+
+        
+    }
+    //user provides end year only
+    if( startYear === "" && endYear !== "")
+    {
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&end_date=${endYear}&api-key=${apikey}`;
+        return returnUrl;
+        
+    }
+    //user provide start year only
+    if( startYear !== "" && endYear === "")
+    {
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&api-key=${apikey}`;
+        return returnUrl;
+        
+    }
+    //user provides both values
+    if( startYear !== "" && endYear !== "")
+    {
+        returnUrl=`${queryURLBase}${searchTerm}&facet=true&begin_date=${startYear}&end_date=${endYear}&api-key=${apikey}`;
+        return returnUrl;
+        
+    }
+   
+    
+}
